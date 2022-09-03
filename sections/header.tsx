32dafa42b4a93a189ce44ade75721af438bfb73a
coolbuhzor <coolbuhzor@gmail.com>
2022-09-03 07:49:10 +0100

    created next app and installed tailwind css

---
+++ sections/header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import NavBar from "../components/nav-bar";
+
+const Header = () => {
+  return <div className="bg-red-500 h-[screen]">Header</div>;
+};
+
+export default Header;
